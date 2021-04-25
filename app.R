f0094f50c8387ed179a6c5b3c9e7b606afd9ac78
JulianUmbhau <jensen_303@hotmail.com>
2021-04-25 23:25:14 +0200

    updating travis

--- app.R
+++ app.R
@@ -1,4 +1,11 @@
 library(shiny)
 
 pkgload::load_all()
+
+
+# første corona relaterede dødsfald
+# - switch imellem cases/deaths
+load(file = "data/data.rda")
+
+
 shinyApp(ui = samlet_UI, server = server)
