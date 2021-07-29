8e7efab5434590936b1b73acdcfa682a92f27580
Julian Johannes Umbhau Jensen <jensen_303@hotmail.com>
2021-07-29 21:58:24 +0200

    removing secrets

--- scripts/rsconnect_upload.R
+++ scripts/rsconnect_upload.R
@@ -12,9 +12,5 @@ setAccountInfo(name="julian-johannes-umbhau-jensen",
                 token=token, # get token through github secrets 
                 secret=secret) # get token through github secrets
 
-# setAccountInfo(name='julian-johannes-umbhau-jensen', 
-#                token='2C37B4019E3B949BE5D490DDAC59A196', 
-#                secret='xst+aZ/6zTvuuNnOcbkVq9NAju+0uPyrqf4yfPoB')
-
 deployApp(getwd(), forceUpdate = T)
 
