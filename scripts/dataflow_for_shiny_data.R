7fd4795f2e1b8e4afbbe0558da6b1d7828a70326
Julian Jensen <jensen_303@hotmail.com>
2022-08-28 16:38:25 +0200

    updates

--- scripts/dataflow_for_shiny_data.R
+++ scripts/dataflow_for_shiny_data.R
@@ -1,13 +1,16 @@
-library(CoronaVisualisation)
+pkgload::load_all()
 
 url_deaths <- "https://raw.githubusercontent.com/CSSEGISandData/COVID-19/master/csse_covid_19_data/csse_covid_19_time_series/time_series_covid19_deaths_global.csv"
 url_confirmed <- "https://raw.githubusercontent.com/CSSEGISandData/COVID-19/master/csse_covid_19_data/csse_covid_19_time_series/time_series_covid19_confirmed_global.csv"
 data_path <- paste0(getwd(), "/data/")
 
 print("Preparing data")
+start_time <- Sys.time()
 corona_data <- data_prep_wrapper(
   url_confirmed,
   url_deaths)
+function_time <- Sys.time()-start_time
+print(paste0("Data prep time: ", function_time))
 
 usethis::use_data(corona_data,
                   overwrite = T)
@@ -21,7 +24,7 @@ samlet_data <- list()
 i <- 1
 load("./data/corona_data.rda")
 start_time <- Sys.time()
-for (country_ in unique(corona_data$collected$country)[1]) {
+for (country_ in unique(corona_data$collected$country)) {
   message(country_," ", i)
   i <- i+1
   country_data <- corona_data$confirmed
@@ -29,12 +32,26 @@ for (country_ in unique(corona_data$collected$country)[1]) {
     dplyr::filter(country == country_)
 #  country_models <- create_country_models(country_data)
   country_models <- train_models(country_data)
+  confirmed <- list(
+    confirmed_data = country_models,
+    confirmed_models = country_data
+  )
+  confirmed <- list(
+    confirmed_data = country_models,
+    confirmed_models = country_data
+  )
+  
+  deaths <- list()
   samlet_data[[country_]] <- list(models = country_models,
                                 data = country_data)
 }
-end_time <- Sys.time()
-print(end_time-start_time)
+function_time <- Sys.time()-start_time
+print(paste0("Data prep time: ", function_time))
 
+print("saving data")
+start_time <- Sys.time()
 usethis::use_data(samlet_data,
                   overwrite = T) # OBS: gives long package load time
+function_time <- Sys.time()-start_time
+print(paste0("Data prep time: ", function_time))
 
